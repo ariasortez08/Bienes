42fa07cad0570e500e25002298617499a2ea7793
ariasortez08 <ariasortez@outlook.com>
2022-10-04 10:23:53 -0600

    Login/Registro

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,9 +1,15 @@
 const formularioLogin = (req, res) => {
-  res.render('auth/login', {});
+  res.render('auth/login', {
+    pagina: 'Login',
+    top: 'Iniciar Sesion',
+  });
 };
 
 const formularioRegistro = (req, res) => {
-  res.render('auth/registro', {});
+  res.render('auth/registro', {
+    pagina: 'Registro',
+    top: 'Registrar Usuario',
+  });
 };
 
 export { formularioLogin, formularioRegistro };
