9786741a70088484d94c831d983c2c697826962c
ariasortez08 <ariasortez@outlook.com>
2022-10-04 00:24:56 -0600

    PrimerPush

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import usuarioRoutes from './router/usuarioRoutes.js';
+
+// ? Creamos la app
+const app = express();
+
+// * INICIAMOS PUG
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+// * CARPETAS PUBLICAS
+
+app.set(express.static('public'));
+
+//Routing
+
+app.use('/auth', usuarioRoutes);
+
+//Definimos el puerto
+
+const port = 4000;
+
+app.listen(port, () => {
+  console.log(`El servidor esta en el puerto ${port}`);
+});
